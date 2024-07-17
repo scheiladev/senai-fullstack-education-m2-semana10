7baccae060a5f841c3b04f34df929d2ccda589c7
Scheila Stihler <sstihler@gmail.com>
2024-07-17 10:02:30 -0300

    feat: criação do card de produto

---
+++ src/app/shared/components/card-produto/card-produto.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-card-produto',
+  standalone: true,
+  imports: [],
+  templateUrl: './card-produto.component.html',
+  styleUrl: './card-produto.component.scss'
+})
+export class CardProdutoComponent {
+
+}
