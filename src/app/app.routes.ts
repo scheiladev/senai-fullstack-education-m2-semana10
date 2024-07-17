3d5b55f17fe1ddf8d064a368ba055900bcd12f48
Scheila Stihler <sstihler@gmail.com>
2024-07-17 10:18:04 -0300

    feat: rota produtos e estrutura básica da página

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,10 +1,15 @@
 import { Routes } from '@angular/router';
 import { HomeComponent } from './pages/home/home.component';
+import { ListagemProdutosComponent } from './pages/listagem-produtos/listagem-produtos.component';
 
 export const routes: Routes = [
   {
     path: 'home',
     component: HomeComponent,
+  },
+  {
+    path: 'produtos',
+    component: ListagemProdutosComponent,
   },
   {
     path: '',
