f24bc9b8d3fb74d049b574cf5ed06e4d579326e5
Scheila Stihler <sstihler@gmail.com>
2024-07-22 20:39:44 -0300

    feat: formatação da listagem de produtos

--- src/app/pages/listagem-produtos/listagem-produtos.component.ts
+++ src/app/pages/listagem-produtos/listagem-produtos.component.ts
@@ -1,12 +1,11 @@
 import { Component } from '@angular/core';
+import { CardProdutoComponent } from '../../shared/components/card-produto/card-produto.component';
 
 @Component({
   selector: 'app-listagem-produtos',
   standalone: true,
-  imports: [],
+  imports: [CardProdutoComponent],
   templateUrl: './listagem-produtos.component.html',
-  styleUrl: './listagem-produtos.component.scss'
+  styleUrl: './listagem-produtos.component.scss',
 })
-export class ListagemProdutosComponent {
-
-}
+export class ListagemProdutosComponent {}
